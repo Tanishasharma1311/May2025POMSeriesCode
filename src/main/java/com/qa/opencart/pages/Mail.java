0d97f430633a8eafcefc23f8bed1f262936b9250
tanisha <sharmatanisha138@gmail.com>
2025-06-10 13:56:26 +0530

    mail naveen

---
+++ src/main/java/com/qa/opencart/pages/Mail.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Mail {
+	
+	public void sendMail(){
+		  System.out.println("mail sent -- Tanisha");
+		}
+	
+	public void getMail(){
+		  System.out.println(" get mail -- Tanisha");
+		}
+
+}
